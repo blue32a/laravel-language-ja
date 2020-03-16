ab1dbe84db313f2afd398f543f0bf3b59bedac90
blue32a <blue32a.jp@gmail.com>
2020-03-16 18:07:24 +0900

    Add ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Blue32a\Laravel\Language;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/lang' => $this->app->resourcePath('lang/'),
+            ], 'blue32a-lang');
+        }
+    }
+}
---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests;
+
+use Blue32a\Laravel\Language\ServiceProvider;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class AzureBlobStorageTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public const PUBLISHE_GROUP = 'blue32a-lang';
+
+    /**
+     * @test
+     */
+    public function testBootRunningInConsole()
+    {
+        $appMock = Mockery::mock('Application');
+        $appMock->shouldReceive('runningInConsole')
+            ->once()
+            ->andReturn(true);
+        $appMock->shouldReceive('resourcePath')
+            ->once()
+            ->andReturn('path');
+
+        $providerMock = Mockery::mock(ServiceProvider::class, [$appMock])
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $providerMock->shouldReceive('publishes')
+            ->once()
+            ->with(Mockery::type('array'), self::PUBLISHE_GROUP);
+
+        $providerMock->boot();
+    }
+
+    /**
+     * @test
+     */
+    public function testBootNotRunningInConsole()
+    {
+        $appMock = Mockery::mock('Application');
+        $appMock->shouldReceive('runningInConsole')
+            ->once()
+            ->andReturn(false);
+
+        $providerMock = Mockery::mock(ServiceProvider::class, [$appMock])
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $providerMock->shouldNotReceive('publishes');
+
+        $providerMock->boot();
+    }
+}
