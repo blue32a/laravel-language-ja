b9cf0df012c0bcba17e972c97b1a1a261b362181
blue32a <blue32a.jp@gmail.com>
2020-03-16 16:33:20 +0900

    Add language resources

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,151 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | バリデーションの言語行
+    |--------------------------------------------------------------------------
+    |
+    | 次の言語行には、バリデータクラスで使用されるデフォルトの
+    | エラーメッセージが含まれています。これらのルールの一部には、
+    | サイズルールなどの複数のバージョンがあります。 
+    | ここでこれらの各メッセージを自由に調整してください。
+    |
+    */
+
+    'accepted' => ':attributeを承認してください。',
+    'active_url' => ':attributeには有効なURLを指定してください。',
+    'after' => ':attributeには:date以降の日付を指定してください。',
+    'after_or_equal' => ':attributeには:dateかそれ以降の日付を指定してください。',
+    'alpha' => ':attributeには英字のみからなる文字列を指定してください。',
+    'alpha_dash' => ':attributeには英数字・ハイフン・アンダースコアのみからなる文字列を指定してください。',
+    'alpha_num' => ':attributeには英数字のみからなる文字列を指定してください。',
+    'array' => ':attributeには配列を指定してください。',
+    'before' => ':attributeには:date以前の日付を指定してください。',
+    'before_or_equal' => ':attributeには:dateかそれ以前の日付を指定してください。',
+    'between' => [
+        'numeric' => ':attributeには:min〜:maxまでの数値を指定してください。',
+        'file' => ':attributeには:min〜:max KBのファイルを指定してください。',
+        'string' => ':attributeには:min〜:max文字の文字列を指定してください。',
+        'array' => ':attributeには:min〜:max個の要素を持つ配列を指定してください。',
+    ],
+    'boolean' => ':attributeには真偽値を指定してください。',
+    'confirmed' => ':attributeが確認用の値と一致しません。',
+    'date' => ':attributeは有効な日付ではありません。',
+    'date_equals' => ':attributeは:dateと同じ日付でなければなりません。',
+    'date_format' => ':attributeは:format形式と一致しません。',
+    'different' => ':attributeには:otherとは異なる値を指定してください。',
+    'digits' => ':attributeは:digits桁の数字でなければなりません。',
+    'digits_between' => ':attributeは:min〜:max桁の数字である必要があります。',
+    'dimensions' => ':attributeの画像サイズが無効です。',
+    'distinct' => ':attributeに指定された値は重複しています。',
+    'email' => ':attributeは有効なメールアドレスでなければなりません。',
+    'ends_with' => ':attributeは、:valuesのいずれかで終了する必要があります。',
+    'exists' => '選択された:attributeは無効です。',
+    'file' => ':attributeはファイルでなければなりません。',
+    'filled' => ':attributeには値が必要です。',
+    'gt' => [
+        'numeric' => ':attributeは:valueより大きくなければなりません。',
+        'file' => ':attributeは:valueキロバイトより大きくなければなりません。',
+        'string' => ':attributeは:value文字より大きくなければなりません。',
+        'array' => ':attributeには:valueより多くのアイテムが必要です。',
+    ],
+    'gte' => [
+        'numeric' => ':attributeは:value以上でなければなりません。',
+        'file' => ':attributeは:valueキロバイト以上でなければなりません。',
+        'string' => ':attributeは:value文字以上でなければなりません。',
+        'array' => ':attributeには:value以上のアイテムが必要です。',
+    ],
+    'image' => ':attributeは画像でなければなりません。',
+    'in' => '選択された:attributeは無効です。',
+    'in_array' => ':attributeは:otherに存在しません。',
+    'integer' => ':attributeは整数でなければなりません。',
+    'ip' => ':attributeは有効なIPアドレスでなければなりません。',
+    'ipv4' => ':attributeは有効なIPv4アドレスでなければなりません。',
+    'ipv6' => ':attributeは有効なIPv6アドレスでなければなりません。',
+    'json' => ':attributeは有効なJSON文字列でなければなりません。',
+    'lt' => [
+        'numeric' => ':attributeは:valueより小さくなければなりません。',
+        'file' => ':attributeは:valueキロバイトより小さくなければなりません。',
+        'string' => ':attributeは:value文字より小さくなければなりません。',
+        'array' => ':attributeには:valueより少ないアイテムが必要です。',
+    ],
+    'lte' => [
+        'numeric' => ':attributeは:value以下でなければなりません。',
+        'file' => ':attributeは:valueキロバイト以下でなければなりません。',
+        'string' => ':attributeは:value文字以下でなければなりません。',
+        'array' => ':attributeには:value以下のアイテムが必要です。',
+    ],
+    'max' => [
+        'numeric' => ':attributeは:maxより大きくてはいけません。',
+        'file' => ':attributeは:maxキロバイトを超えてはいけません。',
+        'string' => ':attributeは:max文字を超えてはいけません。',
+        'array' => ':attributeには:max個を超えるアイテムを含めることはできません。',
+    ],
+    'mimes' => ':attributeは:valuesタイプのファイルでなければなりません。',
+    'mimetypes' => ':attributeは:valuesタイプのファイルでなければなりません。',
+    'min' => [
+        'numeric' => ':attributeは:maxより小さくてはいけません。',
+        'file' => ':attributeは:maxキロバイトより小さくてはいけません。',
+        'string' => ':attributeは:max文字より小さくてはいけません。',
+        'array' => ':attributeには少なくとも:min個のアイテムが必要です。',
+    ],
+    'not_in' => '選択された:attributeは無効です。',
+    'not_regex' => ':attributeは無効な形式です。',
+    'numeric' => ':attributeは数値でなければなりません。',
+    'password' => 'パスワードが間違っています。',
+    'present' => ':attributeが存在する必要があります。',
+    'regex' => ':attributeは無効な形式です。',
+    'required' => ':attributeは必須です。',
+    'required_if' => ':otherが:valueの場合、:attributeは必須です。',
+    'required_unless' => ':otherが:valueではない場合、:attributeは必須です。',
+    'required_with' => ':valuesのうち一つでも存在する場合、:attributeは必須です。',
+    'required_with_all' => ':valuesのうち全て存在する場合、:attributeは必須です。',
+    'required_without' => ':valuesのうちどれか一つでも存在していない場合、:attributeは必須です。',
+    'required_without_all' => ':valuesのうち全て存在していない場合、:attributeは必須です。',
+    'same' => ':attributeと:otherは一致する必要があります。',
+    'size' => [
+        'numeric' => ':attributeは:sizeでなければなりません。',
+        'file' => ':attributeは:sizeキロバイトでなければなりません。',
+        'string' => ':attributeは:size文字でなければなりません。',
+        'array' => ':attributeには:sizeが含まれている必要があります。',
+    ],
+    'starts_with' => ':attributeは:valuesのいずれかで始まる必要があります。',
+    'string' => ':attributeは文字列でなければなりません。',
+    'timezone' => ':attributeは有効なタイムゾーンでなければなりません。',
+    'unique' => ':attributeはすでに使用されています。',
+    'uploaded' => ':attributeのアップロードに失敗しました。',
+    'url' => ':attributeは無効な形式です。',
+    'uuid' => ':attributeは有効なUUIDでなければなりません。',
+
+    /*
+    |--------------------------------------------------------------------------
+    | カスタムバリデーションの言語行
+    |--------------------------------------------------------------------------
+    |
+    | ここでは、「attribute.rule」という規則を使用して行に名前を付けて、
+    | 属性のカスタム検証メッセージを指定できます。 これにより、特定の属性ルールに
+    | 特定のカスタム言語行をすばやく指定できます。
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | カスタムバリデーション属性
+    |--------------------------------------------------------------------------
+    |
+    | 次の言語行を使用して、属性プレースホルダーを「email」ではなく「E-Mail Address」などの
+    | 読みやすいものに置き換えます。 これは単にメッセージをより表現力豊かにするのに役立ちます。
+    |
+    */
+
+    'attributes' => [],
+
+];
